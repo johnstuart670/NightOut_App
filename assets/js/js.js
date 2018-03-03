99ee97a0bf284dbc7472f82e01a830475afb482d
Kevin Smith <kevin@kvnsmith.com>
2018-03-03 16:56:25 -0700

    index update, id declaration in js file

---
+++ assets/js/js.js
@@ -0,0 +1,8 @@
+////// ID's from html////////
+// #modalTital = tital for initial modal window
+// #location = location input search bar
+// #bar = checkbox for bars
+// #restaurant = checkbox for restaurants
+// #datePicker = 
+//
+//
