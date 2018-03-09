d7c973b4fadb34447e552a8407ea61f1f17efdd0
John Stuart <31050027+johnstuart670@users.noreply.github.com>
2018-03-08 19:51:23 -0700

    basic, need to add card

--- assets/js/js.js
+++ assets/js/js.js
@@ -19,72 +19,91 @@ var datePicker;
 
 var modalTital = $('#modalTitle');
 
-var displayEvents =$('#displayEvents');
+var displayEvents = $('#eventDump');
+
+// variables to add to our card
+ var card =  $('div').addClass('card');
+ var cardBody = $('div').addClass("card-body");
+ var cardTitle = $('<h5>').addClass('card-title');
 
 
 // on load of the document
-$(document).ready(function(){
+$(document).ready(function () {
 	// add event listener to the btnStart
-  $('#btnStart').on("click",function(){
-	// keep it from submitting blank
-event.preventDefault();
-// save the information in future variables
-     eventLoc = $('#location').val();
-     datePicker = $('#datePicker').val();
-     console.log(eventLoc);
-     console.log(datePicker);
-// end function
-	var oArgs = {
-		app_key: "dvq7JdvxVKZGZhLq",
-		where: eventLoc,
-		"date": datePicker,
-		page_size: 10,
-		sort_order: "popularity",
-}
-	EVDB.API.call("/events/search", oArgs, function(oData){
-		var eventArray = oData.events.event;
-		console.log(oData);
-		for (var i = 0; i < eventArray.length; i++){
-var eventArr = eventArray[i];
-			var tdO = "<td>";
-			var tdC	= "</td>";
-// create an image
-			var image = eventArr.image.medium;
-			console.log(image);
-			// give it attributes of an src and width
-			var tdImage = $('<img>');
-			tdImage.attr("src", image.url).attr("width",image.width).attr("height", image.height).addClass('img-fluid');
-			var imageTD = tdO + tdImage + tdC;
-// works until 58
-			var artist =(tdO + eventArr.performers.performer[0].name + tdC);
-			var shortBio = (tdO + eventArr.performers.performer[0].short_bio+ tdC);
-			var startTime = (tdO + eventArr.startTime+ tdC);
-			var venue = (tdO + eventArr.venue_name+ tdC);
-
-			var url = eventArr.url;
-			var tdURL = tdO + '<a href = "'+ url + '>Buy Tickets Here</a>' + tdC;
-			// hold as data attributes
-			var longitude = eventArr.longitude;
-			var latitude = evenetArr.latitude;
-// make a new row
-			var newRow = $('<tr>');
-			// do stuff to the newRow
-newRow
-// append with TDs that we built 
-.append(imageTD, artist, shortBio, startTime, venue, tdURL)
-// add a class of i so we reference specific row
-.addClass([i])
-// give data attributes of lat and long to reference in the second API call later
-.data("data-lat", latitude)
-.data("data-long", longitude)
-// append displayEvents with the new Row
-			displayEvents.append(newRow);
-			
+	  $('#btnStart').on("click", function () {
+		// keep it from submitting blank
+		event.preventDefault();
+		// save the information in future variables
+		eventLoc = $('#location').val();
+		datePicker = $('#datePicker').val();
+		console.log(eventLoc);
+		console.log(datePicker);
+		// end function
+		var oArgs = {
+			app_key: "dvq7JdvxVKZGZhLq",
+			where: eventLoc,
+			"date": datePicker,
+			page_size: 10,
+			sort_order: "popularity",
 		}
-		;
-	})
+		EVDB.API.call("/events/search", oArgs, function (oData) {
+			var eventArray = oData.events.event;
+			console.log(oData);
+			for (var i = 0; i < eventArray.length; i++) {
+				var eventArr = eventArray[i];
+				// create an image
+				var image;
+				var tdImage = $('<p>').text("No Image Available");
+				// if the image exists
+				if (eventArr.image) {
+					image = eventArr.image.medium;
+					console.log(image);
+					// give it attributes of an src and width
+					var tdImage = $('<img>')
+						.attr("src", image.url)
+						.attr("width", image.width)
+						.attr("height", image.height)
+						.addClass('img-fluid');
+				};
+				// shortcut variable
+				var performers = eventArr.performers;
+				var artist;
+				// if there are performers
+				if (performers) {
+					if (Array.isArray(performers.performer)) {
+						artist = cardTitle.text(performers.performer[0].name);
+					} else {
+						artist = cardTitle.text(performers.performer.name)
+						;
+					}
+				} else {
+					artist = cardTitle.text(eventArr.title);
+				}
+				var startTime = $("<div>").text(eventArr.startTime);
+				// console.log(startTime);
+				var venue = $("<div>").text(eventArr.venue_name);
+				var url = eventArr.url;
+				var aLink = $('<a>').attr("href", url).text("Learn More Here!");
+				var tdURL = $("<div>").addClass("card-footer").html(aLink);
+				// hold as data attributes
+				var longitude = eventArr.longitude;
+				var latitude = eventArr.latitude;
+				// make a new row
+				var newRow = $('<div>').addClass("row")
+				// do stuff to the newRow
+					// append with TDs that we built 
+					.append(imageTD, artist, shortBio, startTime, venue, tdURL)
+					// add a class of i so we reference specific row
+					.attr("data-number", [i])
+					// give data attributes of lat and long to reference in the second API call later
+					.data("data-lat", latitude)
+					.data("data-long", longitude)
+				// append displayEvents with the new Row
+				displayEvents.append(newRow);
+			}
+		})
 
-})
+	});
 
-// end of the page function
+	// end of the page function
 });
