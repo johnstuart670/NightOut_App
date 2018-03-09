555f1a85cefd93ece117317acc89360ffe7b9744
John Stuart <31050027+johnstuart670@users.noreply.github.com>
2018-03-08 20:05:45 -0700

    ready to test the firstAPI in cards

--- assets/js/js.js
+++ assets/js/js.js
@@ -22,15 +22,19 @@ var modalTital = $('#modalTitle');
 var displayEvents = $('#eventDump');
 
 // variables to add to our card
- var card =  $('div').addClass('card');
- var cardBody = $('div').addClass("card-body");
- var cardTitle = $('<h5>').addClass('card-title');
+var card = $('div').addClass('card');
+var cardBody = $('div').addClass("card-body");
+var cardTitle = $('<h5>').addClass('card-title');
+var cardFooter = $("<div>").addClass("card-footer");
+
+var p9 = $('<p>').addClass('col-md-9');
+var newRow = $('<div>').addClass("row")
 
 
 // on load of the document
 $(document).ready(function () {
 	// add event listener to the btnStart
-	  $('#btnStart').on("click", function () {
+	$('#btnStart').on("click", function () {
 		// keep it from submitting blank
 		event.preventDefault();
 		// save the information in future variables
@@ -51,20 +55,7 @@ $(document).ready(function () {
 			console.log(oData);
 			for (var i = 0; i < eventArray.length; i++) {
 				var eventArr = eventArray[i];
-				// create an image
-				var image;
-				var tdImage = $('<p>').text("No Image Available");
-				// if the image exists
-				if (eventArr.image) {
-					image = eventArr.image.medium;
-					console.log(image);
-					// give it attributes of an src and width
-					var tdImage = $('<img>')
-						.attr("src", image.url)
-						.attr("width", image.width)
-						.attr("height", image.height)
-						.addClass('img-fluid');
-				};
+				// making the card header
 				// shortcut variable
 				var performers = eventArr.performers;
 				var artist;
@@ -74,26 +65,46 @@ $(document).ready(function () {
 						artist = cardTitle.text(performers.performer[0].name);
 					} else {
 						artist = cardTitle.text(performers.performer.name)
-						;
+							;
 					}
 				} else {
 					artist = cardTitle.text(eventArr.title);
 				}
-				var startTime = $("<div>").text(eventArr.startTime);
-				// console.log(startTime);
-				var venue = $("<div>").text(eventArr.venue_name);
+
+				// building items in the row
+				// create an image
+				var image;
+				var tdImage = $('<p>').text("No Image Available");
+				// if the image exists
+				if (eventArr.image) {
+					image = eventArr.image.medium;
+					console.log(image);
+					// give it attributes of an src and width
+					 tdImage = $('<img>')
+						.attr("src", image.url)
+						.attr("width", image.width)
+						.attr("height", image.height)
+						.addClass('img-fluid');
+				};
+				// Log the Start Time in a p class
+				var startTime = p9.text(eventArr.startTime);
+				// log the venue name in a p class
+				var venue = p9.text(eventArr.venue_name);
+				console.log("venue name", eventArr.venue_name);
+
+				// Build a row from the row items of image start time and venue name
+				newRow.append(image, startTime, venue);
+
+				// Build the footer out
 				var url = eventArr.url;
 				var aLink = $('<a>').attr("href", url).text("Learn More Here!");
-				var tdURL = $("<div>").addClass("card-footer").html(aLink);
-				// hold as data attributes
-				var longitude = eventArr.longitude;
-				var latitude = eventArr.latitude;
-				// make a new row
-				var newRow = $('<div>').addClass("row")
-				// do stuff to the newRow
-					// append with TDs that we built 
-					.append(imageTD, artist, shortBio, startTime, venue, tdURL)
-					// add a class of i so we reference specific row
+				var tdURL = cardFooter.html(aLink);
+
+				// build the body of the card
+				cardBody.append(cardTitle, newRow, tdURL);
+				// append the card with the body and
+				card.append(cardBody)
+					// add a class of i so we reference specific card
 					.attr("data-number", [i])
 					// give data attributes of lat and long to reference in the second API call later
 					.data("data-lat", latitude)
