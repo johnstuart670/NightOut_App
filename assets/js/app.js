95306043ca00d7d707b9dc66a873fd655851c8f4
John Stuart <31050027+johnstuart670@users.noreply.github.com>
2018-03-10 13:48:01 -0700

    removed some duplicate variables

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,85 +22,85 @@ google.maps.event.addListener(autocomplete, 'place_changed', function () {
 // End AutoComplete ADD
 
 function initMap() {
-  var map = new google.maps.Map(document.getElementById('mapDump'), {
-    center: {lat: 40.7608, lng: -111.8910},
-    zoom: 13
-  });
-  var card = document.getElementById('pac-card');
-  var input = document.getElementById('location');
+	var map = new google.maps.Map(document.getElementById('mapDump'), {
+		center: { lat: 40.7608, lng: -111.8910 },
+		zoom: 13
+	});
+	var card = document.getElementById('pac-card');
+	var input = document.getElementById('location');
 	var bar = document.getElementById('bar');
 	var restaurants = document.getElementById('restaurant');
-  var strictBounds = document.getElementById('strict-bounds-selector');
+	var strictBounds = document.getElementById('strict-bounds-selector');
 
 
-  var autocomplete = new google.maps.places.Autocomplete(input);
+	var autocomplete = new google.maps.places.Autocomplete(input);
 
-  // Bind the map's bounds (viewport) property to the autocomplete object,
-  // so that the autocomplete requests use the current map bounds for the
-  // bounds option in the request.
-  autocomplete.bindTo('bounds', map);
+	// Bind the map's bounds (viewport) property to the autocomplete object,
+	// so that the autocomplete requests use the current map bounds for the
+	// bounds option in the request.
+	autocomplete.bindTo('bounds', map);
 
-  var infowindow = new google.maps.InfoWindow();
-  var infowindowContent = document.getElementById('infowindow-content');
-  infowindow.setContent(infowindowContent);
-  var marker = new google.maps.Marker({
-    map: map,
-    anchorPoint: new google.maps.Point(0, -29)
-  });
+	var infowindow = new google.maps.InfoWindow();
+	var infowindowContent = document.getElementById('infowindow-content');
+	infowindow.setContent(infowindowContent);
+	var marker = new google.maps.Marker({
+		map: map,
+		anchorPoint: new google.maps.Point(0, -29)
+	});
 
-  autocomplete.addListener('place_changed', function() {
-    infowindow.close();
-    marker.setVisible(false);
-    var place = autocomplete.getPlace();
-    if (!place.geometry) {
-      // User entered the name of a Place that was not suggested and
-      // pressed the Enter key, or the Place Details request failed.
-      window.alert("No details available for input: '" + place.name + "'");
-      return;
-    }
+	autocomplete.addListener('place_changed', function () {
+		infowindow.close();
+		marker.setVisible(false);
+		var place = autocomplete.getPlace();
+		if (!place.geometry) {
+			// User entered the name of a Place that was not suggested and
+			// pressed the Enter key, or the Place Details request failed.
+			window.alert("No details available for input: '" + place.name + "'");
+			return;
+		}
 
-    // If the place has a geometry, then present it on a map.
-    if (place.geometry.viewport) {
-      map.fitBounds(place.geometry.viewport);
-    } else {
-      map.setCenter(place.geometry.location);
-      map.setZoom(17);  // Why 17? Because it looks good.
-    }
-    marker.setPosition(place.geometry.location);
-    marker.setVisible(true);
+		// If the place has a geometry, then present it on a map.
+		if (place.geometry.viewport) {
+			map.fitBounds(place.geometry.viewport);
+		} else {
+			map.setCenter(place.geometry.location);
+			map.setZoom(17);  // Why 17? Because it looks good.
+		}
+		marker.setPosition(place.geometry.location);
+		marker.setVisible(true);
 
-    var address = '';
-    if (place.address_components) {
-      address = [
-        (place.address_components[0] && place.address_components[0].short_name || ''),
-        (place.address_components[1] && place.address_components[1].short_name || ''),
-        (place.address_components[2] && place.address_components[2].short_name || '')
-      ].join(' ');
-    }
+		var address = '';
+		if (place.address_components) {
+			address = [
+				(place.address_components[0] && place.address_components[0].short_name || ''),
+				(place.address_components[1] && place.address_components[1].short_name || ''),
+				(place.address_components[2] && place.address_components[2].short_name || '')
+			].join(' ');
+		}
 
-    infowindowContent.children['place-icon'].src = place.icon;
-    infowindowContent.children['place-name'].textContent = place.name;
-    infowindowContent.children['place-address'].textContent = address;
-    infowindow.open(map, marker);
-  });
+		infowindowContent.children['place-icon'].src = place.icon;
+		infowindowContent.children['place-name'].textContent = place.name;
+		infowindowContent.children['place-address'].textContent = address;
+		infowindow.open(map, marker);
+	});
 
-  // Sets a listener on a radio button to change the filter type on Places
-  // Autocomplete.
-  function setupClickListener(id, types) {
-    var checkbox = document.getElementById(id);
-    checkbox.addEventListener('click', function() {
-      autocomplete.setTypes(types);
-    });
-  }
+	// Sets a listener on a radio button to change the filter type on Places
+	// Autocomplete.
+	function setupClickListener(id, types) {
+		var checkbox = document.getElementById(id);
+		checkbox.addEventListener('click', function () {
+			autocomplete.setTypes(types);
+		});
+	}
 
-  setupClickListener('changetype-bar', ['bar']);
-  setupClickListener('changetype-restaurant', ['restaurant']);
+	setupClickListener('changetype-bar', ['bar']);
+	setupClickListener('changetype-restaurant', ['restaurant']);
 
-  document.getElementById('use-strict-bounds')
-      .addEventListener('click', function() {
-        console.log('Checkbox clicked! New state=' + this.checked);
-        autocomplete.setOptions({strictBounds: this.checked});
-      });
+	document.getElementById('use-strict-bounds')
+		.addEventListener('click', function () {
+			console.log('Checkbox clicked! New state=' + this.checked);
+			autocomplete.setOptions({ strictBounds: this.checked });
+		});
 }
 // globally scoped variables
 var eventLoc;
@@ -112,28 +112,23 @@ var datePicker;
 $(document).ready(function () {
 	$(function () {
 		$('[data-toggle="tooltip"]').tooltip()
-	 })
-	 // Tooltips Initialization
-	 $(function () {
+	})
+	// Tooltips Initialization
+	$(function () {
 		$('[data-toggle="tooltip"]').tooltip()
-	 })
+	})
 
 	var isClass = false;
 	// add event listener to the btnStart
 	$('#btnStart').on("click", function () {
 		// keep it from submitting blank
 		event.preventDefault();
-$('#eventDump').empty();
-if (isClass){
-	$('#eventDump').removeClass(smallEvents)
-	isClass = false;
-}
-
-		var card = $('<div>').addClass('card');
-		var cardBody = $('<div>').addClass("card-body");
-		var cardTitle = $('<h5>').addClass('card-title');
-
-		var newRow = $('<div>').addClass("row")
+		// add a loading gif
+		$('#eventDump').empty();
+		if (isClass) {
+			$('#eventDump').removeClass(smallEvents)
+			isClass = false;
+		}
 		// save the information in future variables
 		eventLoc = $('#location').val();
 		datePicker = $('#datePicker').val();
@@ -152,11 +147,11 @@ if (isClass){
 				var card = $('<div>').addClass('card event animated pulse');
 				var cardBody = $('<div>').addClass('card-body');
 				var cardFooter = $('<button>')
-				.addClass('btn primary-color btn-lg btn-block')
-				.text("Learn More About This Event");
+					.addClass('btn primary-color btn-lg btn-block')
+					.text("Learn More About This Event");
 				var cardTitle = $('<h5>').addClass("card-title");
 				var eventArr = eventArray[i];
-;
+				;
 				// making the card header
 				// shortcut variable
 				var performers = eventArr.performers;
@@ -172,8 +167,6 @@ if (isClass){
 				} else {
 					artist = cardTitle.text(eventArr.title);
 				}
-				// building items in the row
-				var newRow = $('<div>').addClass("row")
 				// create an image
 				var image;
 				var tdImage = $('<img>').attr('src', './assets/images/placeholder.png').addClass("img-fluid");
@@ -192,15 +185,15 @@ if (isClass){
 				var startTime = $('<p>').html(startingTime);
 				// // log the venue name in a p class
 				var venue = $('<p>').html(eventArr.venue_name);
-			var selectEvent = $('<button>')
-			.html("Select this event!")
-			.addClass("selectEvent btn success-color-dark btn-lg btn-block");
+				var selectEvent = $('<button>')
+					.html("Select this event!")
+					.addClass("selectEvent btn success-color-dark btn-lg btn-block");
 				// Build the footer out
 				var url = eventArr.url;
 				var aLink = $('<a>')
-				.attr("href", url)
-				.attr("target", "_blank")
-				.text("Learn More Here!");
+					.attr("href", url)
+					.attr("target", "_blank")
+					.text("Learn More Here!");
 				var tdURL = cardFooter.html(aLink);
 
 				// build the body of the card
