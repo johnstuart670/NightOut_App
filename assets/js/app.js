02131fcea45a1075a8bb85ebf866d0142bb9fe5a
John Stuart <31050027+johnstuart670@users.noreply.github.com>
2018-03-10 21:27:44 -0700

    made functions dry and cleaned up variables

--- assets/js/app.js
+++ assets/js/app.js
@@ -105,8 +105,82 @@ function initMap() {
 // globally scoped variables
 var eventLoc;
 var datePicker;
+var isClass = false;
 
-// initialized the moment.js
+function checkClass(){
+	if (!isClass) {
+	$('#eventDump').removeClass('smallEvents');
+	isClass = true;
+} else {
+	$('#eventDump').addClass('smallEvents');
+	isClass = false;
+}};
+function emptyForm(){
+	$('#location').val('');
+	$('#datePicker').val('');
+}
+
+function cardFactory(event){
+	var card = $('<div>').addClass('card event animated pulse');
+				var cardBody = $('<div>').addClass('card-body');
+				var cardFooter = $('<button>')
+					.addClass('btn primary-color btn-lg btn-block')
+					.text("Learn More About This Event");
+				var cardTitle = $('<h5>').addClass("card-title");
+				// making the card header
+				// shortcut variable
+				var performers = event.performers;
+				var artist;
+				// if there are performers
+				if (performers) {
+					if (Array.isArray(performers.performer)) {
+						artist = cardTitle.text(performers.performer[0].name);
+					} else {
+						artist = cardTitle.text(performers.performer.name)
+							;
+					}
+				} else {
+					artist = cardTitle.text(event.title);
+				}
+				// create an image
+				var image;
+				var tdImage = $('<img>').attr('src', './assets/images/placeholder.png').addClass("img-fluid");
+				// if the image exists
+				if (event.image) {
+					image = event.image.medium;
+					// give it attributes of an src and width
+					tdImage = $('<img>')
+						.attr("src", image.url)
+						.attr("width", image.width)
+						.attr("height", image.height)
+						.addClass('img-fluid');
+				};
+				// Log the Start Time in a p class
+				var startingTime = moment(event.start_time).format("dddd, MMMM Do YYYY, h:mm a");
+				var startTime = $('<p>').html(startingTime);
+				// // log the venue name in a p class
+				var venue = $('<p>').html(event.venue_name);
+				var selectEvent = $('<button>')
+					.html("Select this event!")
+					.addClass("selectEvent btn success-color-dark btn-lg btn-block");
+				// Build the footer out
+				var url = event.url;
+				var aLink = $('<a>')
+					.attr("href", url)
+					.attr("target", "_blank")
+					.text("Learn More Here!");
+				var tdURL = cardFooter.html(aLink);
+
+				// build the body of the card
+				cardBody.append(cardTitle, tdImage, venue, startTime, selectEvent, tdURL);
+				// append the card with the body and
+				card.html(cardBody)
+					// give data attributes of lat and long to reference in the second API call later
+					.attr("data-lat", event.latitude)
+					.attr("data-long", event.longitude);
+				// append displayEvents with the new Row
+				$('#eventDump').append(card);
+}
 
 // on load of the document
 $(document).ready(function () {
@@ -118,21 +192,17 @@ $(document).ready(function () {
 		$('[data-toggle="tooltip"]').tooltip()
 	})
 
-	var isClass = false;
 	// add event listener to the btnStart
 	$('#btnStart').on("click", function () {
 		// keep it from submitting blank
 		event.preventDefault();
 		// add a loading gif
 		$('#eventDump').empty();
-		if (isClass) {
-			$('#eventDump').removeClass(smallEvents)
-			isClass = false;
-		}
+		checkClass();
 		// save the information in future variables
 		eventLoc = $('#location').val();
 		datePicker = $('#datePicker').val();
-		// end function
+// item for running the API call
 		var oArgs = {
 			app_key: "dvq7JdvxVKZGZhLq",
 			where: eventLoc,
@@ -140,76 +210,14 @@ $(document).ready(function () {
 			page_size: 12,
 			sort_order: "popularity",
 		}
+		// the API call
 		EVDB.API.call("/events/search", oArgs, function (oData) {
 			var eventArray = oData.events.event;
 			console.log(eventArray);
 			for (var i = 0; i < 12; i++) {
-				var card = $('<div>').addClass('card event animated pulse');
-				var cardBody = $('<div>').addClass('card-body');
-				var cardFooter = $('<button>')
-					.addClass('btn primary-color btn-lg btn-block')
-					.text("Learn More About This Event");
-				var cardTitle = $('<h5>').addClass("card-title");
-				var eventArr = eventArray[i];
-				;
-				// making the card header
-				// shortcut variable
-				var performers = eventArr.performers;
-				var artist;
-				// if there are performers
-				if (performers) {
-					if (Array.isArray(performers.performer)) {
-						artist = cardTitle.text(performers.performer[0].name);
-					} else {
-						artist = cardTitle.text(performers.performer.name)
-							;
-					}
-				} else {
-					artist = cardTitle.text(eventArr.title);
-				}
-				// create an image
-				var image;
-				var tdImage = $('<img>').attr('src', './assets/images/placeholder.png').addClass("img-fluid");
-				// if the image exists
-				if (eventArr.image) {
-					image = eventArr.image.medium;
-					// give it attributes of an src and width
-					tdImage = $('<img>')
-						.attr("src", image.url)
-						.attr("width", image.width)
-						.attr("height", image.height)
-						.addClass('img-fluid');
-				};
-				// Log the Start Time in a p class
-				var startingTime = moment(eventArr.start_time).format("dddd, MMMM Do YYYY, h:mm a");
-				var startTime = $('<p>').html(startingTime);
-				// // log the venue name in a p class
-				var venue = $('<p>').html(eventArr.venue_name);
-				var selectEvent = $('<button>')
-					.html("Select this event!")
-					.addClass("selectEvent btn success-color-dark btn-lg btn-block");
-				// Build the footer out
-				var url = eventArr.url;
-				var aLink = $('<a>')
-					.attr("href", url)
-					.attr("target", "_blank")
-					.text("Learn More Here!");
-				var tdURL = cardFooter.html(aLink);
-
-				// build the body of the card
-				cardBody.append(cardTitle, tdImage, venue, startTime, selectEvent, tdURL);
-				// append the card with the body and
-				card.html(cardBody)
-					// add a class of i so we reference specific card
-					.attr("data-number", [i])
-					// give data attributes of lat and long to reference in the second API call later
-					.attr("data-lat", eventArr.latitude)
-					.attr("data-long", eventArr.longitude);
-				// append displayEvents with the new Row
-				$('#eventDump').append(card);
+				cardFactory(eventArray[i]);
 			};
 		});
-		// put in a reset function
 	});
 	// end of the page function
 });
