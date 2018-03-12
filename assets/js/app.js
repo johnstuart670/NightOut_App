e511ab76cf27863df47f5bd344db0b3d59956a50
gbert254 <gbert254@gmail.com>
2018-03-12 04:20:53 -0600

    google map api

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,95 +8,74 @@
 // #mapDump = data from google places
 
 //-------------------------------------//
-// AutoComplete - Joe
-var input = $('#location')[0];
-var autocomplete = new google.maps.places.Autocomplete(input, { types: ['(cities)'] });
-google.maps.event.addListener(autocomplete, 'place_changed', function () {
-	var place = autocomplete.getPlace();
-})
+// // AutoComplete - Joe
+function activatePlacesSearch() {
+	var input = document.getElementById('location');
+	var autocomplete = new google.maps.places.Autocomplete(input);
+}
 // End AutoComplete ADD
 
-function initMap() {
-	var map = new google.maps.Map(document.getElementById('mapDump'), {
-		center: { lat: 40.7608, lng: -111.8910 },
-		zoom: 13
-	});
-	var card = document.getElementById('pac-card');
-	var input = document.getElementById('location');
-	var bar = document.getElementById('bar');
-	var restaurants = document.getElementById('restaurant');
-	var strictBounds = document.getElementById('strict-bounds-selector');
+var map;
 
 
-	var autocomplete = new google.maps.places.Autocomplete(input);
-
-	// Bind the map's bounds (viewport) property to the autocomplete object,
-	// so that the autocomplete requests use the current map bounds for the
-	// bounds option in the request.
-	autocomplete.bindTo('bounds', map);
-
-	var infowindow = new google.maps.InfoWindow();
-	var infowindowContent = document.getElementById('infowindow-content');
-	infowindow.setContent(infowindowContent);
-	var marker = new google.maps.Marker({
-		map: map,
-		anchorPoint: new google.maps.Point(0, -29)
+$(document).on("click", ".selectEvent", function () {
+	var longitude = $(this).attr("data-long");
+	var latitude = $(this).attr("data-lat");
+	var lat = parseFloat(latitude);
+	var lng = parseFloat(longitude);
+	console.log(lat);
+	console.log(lng);
+	// Create the map
+	var startLoc = { lat, lng };
+	console.log(startLoc);
+	map = new google.maps.Map(document.getElementById('mapDump'), {
+		center: startLoc,
+		zoom: 17
 	});
 
-	autocomplete.addListener('place_changed', function () {
-		infowindow.close();
-		marker.setVisible(false);
-		var place = autocomplete.getPlace();
-		if (!place.geometry) {
-			// User entered the name of a Place that was not suggested and
-			// pressed the Enter key, or the Place Details request failed.
-			window.alert("No details available for input: '" + place.name + "'");
-			return;
+	//Create the places service
+	var service = new google.maps.places.PlacesService(map);
+
+	// Perform a nearby search
+	service.nearbySearch(
+		{ location: startLoc, radius: 1500, type: ['restaurant'] },
+		function (results, status, pagination) {
+			if (status !== 'OK') return;
+			console.log(results);
+			createMarkers(results);
+
 		}
+	)
+	function createMarkers(places) {
+		var bounds = new google.maps.LatLngBounds();
+		var placesList = document.getElementById('placeDump');
 
-		// If the place has a geometry, then present it on a map.
-		if (place.geometry.viewport) {
-			map.fitBounds(place.geometry.viewport);
-		} else {
-			map.setCenter(place.geometry.location);
-			map.setZoom(17);  // Why 17? Because it looks good.
+		for (var i = 0, place; place = place[i]; i++) {
+			var picture = {
+				url: place.icon,
+				size: new google.maps.Size(71, 71),
+				origin: new google.maps.Point(0, 0),
+				anchor: new google.maps.Point(17, 34),
+				scaledSize: new google.maps.Size(25, 25)
+			};
+
+			var marker = new google.maps.Marker({
+				map: map,
+				icon: image,
+				title: place, name,
+				position: place.geometry.location
+			});
+
+			var li = document.createElement('li');
+			li.textContent = place.name;
+			placesList.appendChild(li);
+
+			bounds.extend(place.geometry.location);
 		}
-		marker.setPosition(place.geometry.location);
-		marker.setVisible(true);
-
-		var address = '';
-		if (place.address_components) {
-			address = [
-				(place.address_components[0] && place.address_components[0].short_name || ''),
-				(place.address_components[1] && place.address_components[1].short_name || ''),
-				(place.address_components[2] && place.address_components[2].short_name || '')
-			].join(' ');
-		}
-
-		infowindowContent.children['place-icon'].src = place.icon;
-		infowindowContent.children['place-name'].textContent = place.name;
-		infowindowContent.children['place-address'].textContent = address;
-		infowindow.open(map, marker);
-	});
-
-	// Sets a listener on a radio button to change the filter type on Places
-	// Autocomplete.
-	function setupClickListener(id, types) {
-		var checkbox = document.getElementById(id);
-		checkbox.addEventListener('click', function () {
-			autocomplete.setTypes(types);
-		});
+		map.fitBounds(bounds);
 	}
+})
 
-	setupClickListener('changetype-bar', ['bar']);
-	setupClickListener('changetype-restaurant', ['restaurant']);
-
-	document.getElementById('use-strict-bounds')
-		.addEventListener('click', function () {
-			console.log('Checkbox clicked! New state=' + this.checked);
-			autocomplete.setOptions({ strictBounds: this.checked });
-		});
-}
 // globally scoped variables
 var eventLoc;
 var datePicker;
@@ -208,12 +187,12 @@ function cardFactoryEvents(event) {
 var loadGifDiv = $('<div>')
 	.addClass("loadingGif")
 	.html(
-	$('<div>')
-		.html(
-		$('<img>')
-			.attr('src', './assets/images/loading.gif')
-			.addClass('whiteBG')
-		));
+		$('<div>')
+			.html(
+				$('<img>')
+					.attr('src', './assets/images/loading.gif')
+					.addClass('whiteBG')
+			));
 
 // function to have a loading Gif
 function loadingGif(div) {
@@ -267,6 +246,7 @@ $(document).ready(function () {
 				}
 			}
 		});
+
 	});
 	// on click of the resetBtn
 	$('#resetBtn').click(function () {
